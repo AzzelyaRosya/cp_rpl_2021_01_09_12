0ae7303e0949a0014832841d0d7076c273e3b023
AzzelyaR <azzelya.denovya@gmail.com>
2022-08-16 15:32:15 +0700

    footer update awal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import Footer from './pages/Footer/index';
+
 function App() {
   return (
-    <div className="App">
-      <p>anjay adel bau tai</p>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        minHeight: '100vh',
+      }}
+    >
+      <nav>Navbar</nav>
+      <main>Main Content</main>
+      <Footer />
     </div>
   );
 }
