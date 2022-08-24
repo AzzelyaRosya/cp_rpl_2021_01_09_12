2fd9166cb5359265c13ebb86351865235ee1b8db
AzzelyaR <azzelya.denovya@gmail.com>
2022-08-24 10:31:48 +0700

    update package lock json

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -36,7 +36,7 @@ import pd3 from '../../assets/img/pd3.jpg';
 import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 // style
-import { qr, qrImg } from './styles';
+import { borderRadiusImages, cardStyle, typoCardStyle, flexCard, flexGrid, radiusVideo } from './styles';
 
 export default function Products() {
   const [open1, setOpen1] = React.useState(false);
@@ -71,7 +71,7 @@ export default function Products() {
 
   return (
     <>
-      <img src={bannerproduct} style={{ width: '100%', height: 400 }} />
+      <img src={bannerproduct} style={{ width: '100%', height: 400, objectFit: 'cover' }} />
 
       <Box sx={{ align: 'center' }} style={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
         <Grid>
@@ -79,62 +79,29 @@ export default function Products() {
         </Grid>
 
         <Grid sx={{ display: 'flex', justifyContent: 'center', pb: 5 }}>
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=laW8rtUEMyc"
-            width="81%"
-            style={{
-              //border radius img
-              borderBottomLeftRadius: 5,
-              borderBottomRightRadius: 5,
-              borderTopRightRadius: 5,
-              borderTopLeftRadius: 5,
-              overflow: 'hidden',
-            }}
-          />
+          <ReactPlayer url="https://www.youtube.com/watch?v=laW8rtUEMyc" width="81%" style={radiusVideo} />
         </Grid>
       </Box>
 
-      <Grid container spacing={2} sx={{ pb: 10, display: 'flex', justifyContent: 'center' }}>
+      <Grid container spacing={2} pb={10} sx={flexGrid}>
         {/* Products 1 */}
-        <Grid item xs={3.3} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Card
-            sx={{ maxWidth: 320, bgcolor: '#9AAFDF' }}
-            style={{
-              //border radius img
-              borderBottomLeftRadius: 8,
-              borderBottomRightRadius: 8,
-              borderTopRightRadius: 8,
-              borderTopLeftRadius: 8,
-              overflow: 'hidden',
-            }}
-          >
+        <Grid item md={3.3} sm={12} xs={12} sx={flexGrid}>
+          <Card sx={cardStyle} style={borderRadiusImages}>
             <img src={pd1} style={{ width: '100%', maxHeight: 200 }} />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div" sx={{ color: '#36476B', fontWeight: 600, textAlign: 'center' }}>
+              <Typography gutterBottom variant="h5" component="div" sx={typoCardStyle}>
                 TITLE PRODUCTS
               </Typography>
               <Typography variant="body2" color="#FFFFFF" sx={{ textTransform: 'lowercase' }}>
                 LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET
               </Typography>
             </CardContent>
-            <CardActions sx={{ display: 'flex', justifyContent: 'right' }}>
-              <Button
-                variant="contained"
-                onClick={handleClickOpen1}
-                sx={{ bgcolor: '#6D8CD0' }}
-                style={{
-                  //border radius img
-                  borderBottomLeftRadius: 8,
-                  borderBottomRightRadius: 8,
-                  borderTopRightRadius: 8,
-                  borderTopLeftRadius: 8,
-                  overflow: 'hidden',
-                }}
-              >
+            <CardActions sx={flexCard}>
+              <Button variant="contained" onClick={handleClickOpen1} sx={{ bgcolor: '#6D8CD0' }} style={borderRadiusImages}>
                 <ArrowRightAltIcon />
               </Button>
               <Dialog fullScreen={fullScreen} open={open1} onClose={handleClose1}>
-                <DialogTitle sx={{ color: '#36476B', fontWeight: 600, textAlign: 'center' }}>TITLE PRODUCTS 1 </DialogTitle>
+                <DialogTitle sx={typoCardStyle}>TITLE PRODUCTS 1 </DialogTitle>
                 <DialogContent>
                   <DialogContentText>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -148,45 +115,23 @@ export default function Products() {
         </Grid>
 
         {/* Products 2 */}
-        <Grid item xs={3.3} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Card
-            sx={{ maxWidth: 320, bgcolor: '#9AAFDF' }}
-            style={{
-              //border radius img
-              borderBottomLeftRadius: 8,
-              borderBottomRightRadius: 8,
-              borderTopRightRadius: 8,
-              borderTopLeftRadius: 8,
-              overflow: 'hidden',
-            }}
-          >
+        <Grid item md={3.3} sm={12} xs={12} sx={flexGrid}>
+          <Card sx={cardStyle} style={borderRadiusImages}>
             <img src={pd2} style={{ width: '100%', maxHeight: 200 }} />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div" sx={{ color: '#36476B', fontWeight: 600, textAlign: 'center' }}>
+              <Typography gutterBottom variant="h5" component="div" sx={typoCardStyle}>
                 TITLE PRODUCTS
               </Typography>
               <Typography variant="body2" color="#FFFFFF" sx={{ textTransform: 'lowercase' }}>
                 LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET
               </Typography>
             </CardContent>
-            <CardActions sx={{ display: 'flex', justifyContent: 'right' }}>
-              <Button
-                variant="contained"
-                onClick={handleClickOpen2}
-                sx={{ bgcolor: '#6D8CD0' }}
-                style={{
-                  //border radius img
-                  borderBottomLeftRadius: 8,
-                  borderBottomRightRadius: 8,
-                  borderTopRightRadius: 8,
-                  borderTopLeftRadius: 8,
-                  overflow: 'hidden',
-                }}
-              >
+            <CardActions sx={flexCard}>
+              <Button variant="contained" onClick={handleClickOpen2} sx={{ bgcolor: '#6D8CD0' }} style={borderRadiusImages}>
                 <ArrowRightAltIcon />
               </Button>
               <Dialog fullScreen={fullScreen} open={open2} onClose={handleClose2}>
-                <DialogTitle sx={{ color: '#36476B', fontWeight: 600, textAlign: 'center' }}>TITLE PRODUCTS 2 </DialogTitle>
+                <DialogTitle sx={typoCardStyle}>TITLE PRODUCTS 2 </DialogTitle>
                 <DialogContent>
                   <DialogContentText>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -200,45 +145,23 @@ export default function Products() {
         </Grid>
 
         {/* Products 3 */}
-        <Grid item xs={3.3} sx={{ display: 'flex', justifyContent: 'center' }}>
-          <Card
-            sx={{ maxWidth: 320, bgcolor: '#9AAFDF' }}
-            style={{
-              //border radius img
-              borderBottomLeftRadius: 8,
-              borderBottomRightRadius: 8,
-              borderTopRightRadius: 8,
-              borderTopLeftRadius: 8,
-              overflow: 'hidden',
-            }}
-          >
+        <Grid item md={3.3} sm={12} xs={12} sx={flexGrid}>
+          <Card sx={cardStyle} style={borderRadiusImages}>
             <img src={pd3} style={{ width: '100%', maxHeight: 200 }} />
             <CardContent>
-              <Typography gutterBottom variant="h5" component="div" sx={{ color: '#36476B', fontWeight: 600, textAlign: 'center' }}>
+              <Typography gutterBottom variant="h5" component="div" sx={typoCardStyle}>
                 TITLE PRODUCTS
               </Typography>
               <Typography variant="body2" color="#FFFFFF" sx={{ textTransform: 'lowercase' }}>
                 LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET
               </Typography>
             </CardContent>
-            <CardActions sx={{ display: 'flex', justifyContent: 'right' }}>
-              <Button
-                variant="contained"
-                onClick={handleClickOpen3}
-                sx={{ bgcolor: '#6D8CD0' }}
-                style={{
-                  //border radius img
-                  borderBottomLeftRadius: 8,
-                  borderBottomRightRadius: 8,
-                  borderTopRightRadius: 8,
-                  borderTopLeftRadius: 8,
-                  overflow: 'hidden',
-                }}
-              >
+            <CardActions sx={flexCard}>
+              <Button variant="contained" onClick={handleClickOpen3} sx={{ bgcolor: '#6D8CD0' }} style={borderRadiusImages}>
                 <ArrowRightAltIcon />
               </Button>
               <Dialog fullScreen={fullScreen} open={open3} onClose={handleClose3}>
-                <DialogTitle sx={{ color: '#36476B', fontWeight: 600, textAlign: 'center' }}>TITLE PRODUCTS 3 </DialogTitle>
+                <DialogTitle sx={typoCardStyle}>TITLE PRODUCTS 3 </DialogTitle>
                 <DialogContent>
                   <DialogContentText>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
