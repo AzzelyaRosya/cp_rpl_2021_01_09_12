a9922628411591e573a387e4eae2d6f49b73c853
AzzelyaR <azzelya.denovya@gmail.com>
2022-09-02 11:31:42 +0700

    update ya

--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import {
   Box,
   Typography,
@@ -21,38 +21,31 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-} from "@mui/material";
-import ReactPlayer from "react-player";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import { useTheme } from "@mui/material/styles";
+} from '@mui/material';
+import ReactPlayer from 'react-player';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
 
-// img
-import bannerproduct from "../../assets/img/bannerproduct.png";
-import pd1 from "../../assets/img/pd1.jpg";
-import pd2 from "../../assets/img/pd2.jpg";
-import pd3 from "../../assets/img/pd3.jpg";
+// img and video
+import bannerproduct from '../../assets/img/bannerproduct.png';
+import pd1 from '../../assets/img/pd1.jpg';
+import pd2 from '../../assets/img/pd2.jpg';
+import pd3 from '../../assets/img/pd3.jpg';
+import apaaja from '../../assets/video/contohvideo.mp4';
 
 // icons
-import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
 
 // style
-import {
-  borderRadiusImages,
-  cardStyle,
-  typoCardStyle,
-  flexCard,
-  flexGrid,
-  radiusVideo,
-  buttonHover,
-} from "./styles";
-import Colors from "../../theme/Color";
+import { borderRadiusImages, cardStyle, typoCardStyle, flexCard, flexGrid, radiusVideo, buttonHover } from './styles';
+import Colors from '../../theme/Color';
 
 export default function Products() {
   const [open1, setOpen1] = React.useState(false);
   const [open2, setOpen2] = React.useState(false);
   const [open3, setOpen3] = React.useState(false);
   const theme = useTheme();
-  const fullScreen = useMediaQuery(theme.breakpoints.down("md"));
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
 
   const handleClickOpen1 = () => {
     setOpen1(true);
@@ -80,17 +73,14 @@ export default function Products() {
 
   return (
     <>
-      <img
-        src={bannerproduct}
-        style={{ width: "100%", height: 400, objectFit: "cover" }}
-      />
+      <img src={bannerproduct} style={{ width: '100%', height: 400, objectFit: 'cover' }} />
 
       <Box
-        sx={{ align: "center" }}
+        sx={{ align: 'center' }}
         style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
         }}
       >
         <Grid>
@@ -99,7 +89,7 @@ export default function Products() {
               color: Colors.darkBlue,
               fontWeight: 700,
               fontSize: 30,
-              textAlign: "center",
+              textAlign: 'center',
               mb: 3,
               mt: 3,
             }}
@@ -108,9 +98,13 @@ export default function Products() {
           </Typography>
         </Grid>
 
-        <Grid sx={{ display: "flex", justifyContent: "center", pb: 5 }}>
-          <ReactPlayer
-            url="https://www.youtube.com/watch?v=laW8rtUEMyc"
+        <Grid sx={{ display: 'flex', justifyContent: 'center', pb: 5 }}>
+          <video
+            // autoPlay="true"
+            controls
+            type="video/mp4"
+            className="video"
+            src={apaaja}
             width="81%"
             style={radiusVideo}
           />
@@ -121,51 +115,26 @@ export default function Products() {
         {/* Products 1 */}
         <Grid item md={3.3} sm={12} xs={12} sx={flexGrid}>
           <Card sx={cardStyle} style={borderRadiusImages}>
-            <img src={pd1} style={{ width: "100%", maxHeight: 200 }} />
+            <img src={pd1} style={{ width: '100%', maxHeight: 200 }} />
             <CardContent>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                sx={typoCardStyle}
-              >
+              <Typography gutterBottom variant="h5" component="div" sx={typoCardStyle}>
                 TITLE PRODUCTS
               </Typography>
-              <Typography
-                variant="body2"
-                color="#FFFFFF"
-                sx={{ textTransform: "lowercase" }}
-              >
-                LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM
-                DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT
-                AMETLOREM IPSUM DOLOR SIT AMET
+              <Typography variant="body2" color="#FFFFFF" sx={{ textTransform: 'lowercase' }}>
+                LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET
               </Typography>
             </CardContent>
             <CardActions sx={flexCard}>
-              <Button
-                variant="contained"
-                onClick={handleClickOpen1}
-                sx={buttonHover}
-                style={borderRadiusImages}
-              >
+              <Button variant="contained" onClick={handleClickOpen1} sx={buttonHover} style={borderRadiusImages}>
                 <ArrowRightAltIcon />
               </Button>
-              <Dialog
-                fullScreen={fullScreen}
-                open={open1}
-                onClose={handleClose1}
-              >
+              <Dialog fullScreen={fullScreen} open={open1} onClose={handleClose1}>
                 <DialogTitle sx={typoCardStyle}>TITLE PRODUCTS 1 </DialogTitle>
                 <DialogContent>
                   <DialogContentText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                    laborum.
                   </DialogContentText>
                 </DialogContent>
               </Dialog>
@@ -176,51 +145,26 @@ export default function Products() {
         {/* Products 2 */}
         <Grid item md={3.3} sm={12} xs={12} sx={flexGrid}>
           <Card sx={cardStyle} style={borderRadiusImages}>
-            <img src={pd2} style={{ width: "100%", maxHeight: 200 }} />
+            <img src={pd2} style={{ width: '100%', maxHeight: 200 }} />
             <CardContent>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                sx={typoCardStyle}
-              >
+              <Typography gutterBottom variant="h5" component="div" sx={typoCardStyle}>
                 TITLE PRODUCTS
               </Typography>
-              <Typography
-                variant="body2"
-                color="#FFFFFF"
-                sx={{ textTransform: "lowercase" }}
-              >
-                LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM
-                DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT
-                AMETLOREM IPSUM DOLOR SIT AMET
+              <Typography variant="body2" color="#FFFFFF" sx={{ textTransform: 'lowercase' }}>
+                LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET
               </Typography>
             </CardContent>
             <CardActions sx={flexCard}>
-              <Button
-                variant="contained"
-                onClick={handleClickOpen2}
-                sx={buttonHover}
-                style={borderRadiusImages}
-              >
+              <Button variant="contained" onClick={handleClickOpen2} sx={buttonHover} style={borderRadiusImages}>
                 <ArrowRightAltIcon />
               </Button>
-              <Dialog
-                fullScreen={fullScreen}
-                open={open2}
-                onClose={handleClose2}
-              >
+              <Dialog fullScreen={fullScreen} open={open2} onClose={handleClose2}>
                 <DialogTitle sx={typoCardStyle}>TITLE PRODUCTS 2 </DialogTitle>
                 <DialogContent>
                   <DialogContentText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                    laborum.
                   </DialogContentText>
                 </DialogContent>
               </Dialog>
@@ -231,51 +175,26 @@ export default function Products() {
         {/* Products 3 */}
         <Grid item md={3.3} sm={12} xs={12} sx={flexGrid}>
           <Card sx={cardStyle} style={borderRadiusImages}>
-            <img src={pd3} style={{ width: "100%", maxHeight: 200 }} />
+            <img src={pd3} style={{ width: '100%', maxHeight: 200 }} />
             <CardContent>
-              <Typography
-                gutterBottom
-                variant="h5"
-                component="div"
-                sx={typoCardStyle}
-              >
+              <Typography gutterBottom variant="h5" component="div" sx={typoCardStyle}>
                 TITLE PRODUCTS
               </Typography>
-              <Typography
-                variant="body2"
-                color="#FFFFFF"
-                sx={{ textTransform: "lowercase" }}
-              >
-                LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM
-                DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT
-                AMETLOREM IPSUM DOLOR SIT AMET
+              <Typography variant="body2" color="#FFFFFF" sx={{ textTransform: 'lowercase' }}>
+                LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMET LOREM IPSUM DOLOR SIT AMETLOREM IPSUM DOLOR SIT AMET
               </Typography>
             </CardContent>
             <CardActions sx={flexCard}>
-              <Button
-                variant="contained"
-                onClick={handleClickOpen3}
-                sx={buttonHover}
-                style={borderRadiusImages}
-              >
+              <Button variant="contained" onClick={handleClickOpen3} sx={buttonHover} style={borderRadiusImages}>
                 <ArrowRightAltIcon />
               </Button>
-              <Dialog
-                fullScreen={fullScreen}
-                open={open3}
-                onClose={handleClose3}
-              >
+              <Dialog fullScreen={fullScreen} open={open3} onClose={handleClose3}>
                 <DialogTitle sx={typoCardStyle}>TITLE PRODUCTS 3 </DialogTitle>
                 <DialogContent>
                   <DialogContentText>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                    do eiusmod tempor incididunt ut labore et dolore magna
-                    aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                    ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit
-                    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                    occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+                    laborum.
                   </DialogContentText>
                 </DialogContent>
               </Dialog>
