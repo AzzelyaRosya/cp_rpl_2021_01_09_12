463e80023db15558864c245fee55b5eb320be1ab
AzzelyaR <azzelya.denovya@gmail.com>
2022-09-30 12:54:00 +0700

    Updated

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import HomePages from './pages/Homepages/index';
 import About from './pages/About/index';
 import Products from './pages/Products/index';
 import Footer from './components/Footer/index';
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 function App() {
   return (
@@ -14,8 +14,8 @@ function App() {
 
         <Switch>
           <Route path="/cp_rpl_2021_01_09_12/">
-            <Redirect exact to="/cp_rpl_2021_01_09_12/" />
-            <Route path="/cp_rpl_2021_01_09_12/" component={HomePages} />
+            <Redirect exact to="/home" />
+            <Route path="/home" component={HomePages} />
           </Route>
           <Route path="/about/" component={About} />
           <Route path="/product/" component={Products} />
